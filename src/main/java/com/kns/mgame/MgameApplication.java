8aa9c6df5a1330441cad4e324282981cda311c70
Abhishek Kumar <abhishek.kumar.external@telefonica.com>
2021-10-10 12:05:55 +0200

    Initial commit

---
+++ src/main/java/com/kns/mgame/MgameApplication.java
@@ -0,0 +1,15 @@
+package com.kns.mgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgameApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/kns/mgame/MgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kns.mgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
